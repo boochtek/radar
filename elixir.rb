7866715738d6ecef238ecb4e7949325e32916ec8
Craig Buchek <github@craigbuchek.com>
2018-12-17 22:23:12 -0600

    Create elixir.rb
    Pulled from existing notes.

---
+++ elixir.rb
@@ -0,0 +1,10 @@
+Elixir
+======
+
+* [Credo][https://github.com/rrrene/credo] static code analysis
+* [Distillery](https://github.com/bitwalker/distillery) for deployment
+    * Replacement for Exrm (and Edeliver)
+    * Supports hot upgrades
+    * Nice blog article: https://crypt.codemancers.com/posts/2016-10-06-elixir-phoenix-distillery/
+* [Poison](https://github.com/devinus/poison) JSON parser/encoder
+    * Supposed to be very fast
